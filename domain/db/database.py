39d0c48aa56d92fab7eae4278e9c511b1be48fa2
Ural Kozhokmatov <ural@Urals-MacBook-Pro.local>
2026-02-28 01:29:58 -0800

    db up

--- domain/db/database.py
+++ domain/db/database.py
@@ -1,6 +1,10 @@
 import os
 from sqlalchemy import create_engine, text
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import sessionmaker
+try:
+    from sqlalchemy.orm import declarative_base
+except ImportError:
+    from sqlalchemy.ext.declarative import declarative_base
 from dotenv import load_dotenv
 
 
